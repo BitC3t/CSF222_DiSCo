8c74e9aba205b418563673176586f3015c39f692
Druva D <ddruva445@gmail.com>
2023-11-26 15:34:23 +0530

    Improved hungarian

--- hungarian.py
+++ hungarian.py
@@ -19,6 +19,33 @@ def plotGraph(graph, ax, title):
 
 
 input = input_graph.get_data()
+
+
+def graph_to_json(G):
+    json_data = {}
+    json_data["professors"] = set()
+    json_data["courses"] = set()
+    json_data["prof_data"] = {}
+    json_data["course_data"] = {}
+    for prof, course in G.edges():
+        json_data["professors"].add(prof)
+        json_data["courses"].add(course)
+        if prof in json_data["prof_data"]:
+            json_data["prof_data"][prof].append(course)
+        else:
+            json_data["prof_data"][prof] = [course]
+        if course in json_data["course_data"]:
+            json_data["course_data"][course].append(prof)
+        else:
+            json_data["course_data"][course] = [prof]
+    json_data["professors"] = list(json_data["professors"])
+    json_data["courses"] = list(json_data["courses"])
+    for prof, data in input["prof_data"].items():
+        json_data["prof_data"][prof].insert(0, data[1])
+        json_data["prof_data"][prof].insert(0, data[0])
+    return json_data
+
+
 G = nx.Graph()
 
 # Adding all professors
@@ -26,7 +53,6 @@ G.add_nodes_from(
     [(i, prof) for i, prof in enumerate(input["prof_data"].keys())],
     bipartite=0,
 )
-print(G.nodes())
 
 G.add_nodes_from(
     [
@@ -38,7 +64,6 @@ G.add_nodes_from(
     ],
     bipartite=0,
 )
-print(G.nodes())
 
 G.add_nodes_from(
     [
@@ -50,7 +75,7 @@ G.add_nodes_from(
     ],
     bipartite=0,
 )
-print(G.nodes())
+profs = len(G.nodes())
 # Multiply by two because of two slots
 G.add_nodes_from(
     [
@@ -69,6 +94,10 @@ G.add_nodes_from(
     ],
     bipartite=1,
 )
+courses = len(G.nodes()) - profs
+
+if profs != courses:
+    raise Exception("Professors and courses are not equal")
 
 labels = {}
 for node in G.nodes():
@@ -88,22 +117,66 @@ for prof, data in input["prof_data"].items():
 
 # print(G.edges())
 
-match2 = nx.bipartite.maximum_matching(G)
-g_match2 = nx.Graph()
-for kk, vv in match2.items():
-    g_match2.add_edge(kk, vv)
-match2 = nx.max_weight_matching(G, maxcardinality=True)
-g_match = nx.Graph()
-print(match2)
-for edge in match2:
-    prof = edge[0][1]
-    course = edge[1][1]
-    g_match.add_edge(prof, course)
+
+def all_maximal_matchings(T):
+    maximal_matchings = []
+    partial_matchings = [{(u, v)} for (u, v) in T.edges()]
+
+    i = 0
+    while partial_matchings:
+        i += 1
+        print(i)
+        # get current partial matching
+        m = partial_matchings.pop()
+        nodes_m = set(itertools.chain(*m))
+
+        extended = False
+        for u, v in T.edges():
+            if u not in nodes_m and v not in nodes_m:
+                extended = True
+                # copy m, extend it and add it to the list of partial matchings
+                m_extended = set(m)
+                m_extended.add((u, v))
+                partial_matchings.append(m_extended)
+
+        if not extended and m not in maximal_matchings:
+            maximal_matchings.append(m)
+
+    return maximal_matchings
+
 
 from matplotlib import pyplot as plt
 
+import time
+
+start = time.time()
+# matches = all_maximal_matchings(G)
+# matches = nx.max_weight_matching(G, maxcardinality=True)
+matches = nx.bipartite.maximum_matching(G)
+print(time.time() - start)
+print(matches)
+matches = [matches]
+g_match2 = nx.Graph()
+for match2 in matches:
+    for kk, vv in match2:
+        g_match2.add_edge(kk, vv)
+    match2 = nx.max_weight_matching(G, maxcardinality=True)
+    g_match = nx.DiGraph()
+    print("edges")
+    for edge in match2:
+        prof = edge[1][1]
+        course = edge[0][1]
+        print(prof, course)
+        g_match.add_edge(prof, course)
+    print(*g_match.edges(), sep="\n")
+    nx.draw(g_match, with_labels=True)
+    # nx.draw(G, with_labels=True)
+    plt.show()
+    import json
+
+    with open("output.json", "w") as f:
+        json.dump(graph_to_json(g_match), f, indent=4)
+    print("done")
+
 
 # nx.draw(G, with_labels=True)
-nx.draw(g_match, with_labels=True)
-# nx.draw(G, with_labels=True)
-plt.show()
