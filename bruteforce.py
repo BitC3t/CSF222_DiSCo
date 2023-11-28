a2c24589b2932ba0b5dd47b853e93c88aca6f59c
Kushagra Malviya <117557245+darthlazius@users.noreply.github.com>
2023-11-28 04:42:56 +0000

    Update bruteforce.py
    fixed taking output in a json format

--- bruteforce.py
+++ bruteforce.py
@@ -1,13 +1,14 @@
 import json
 from itertools import product
+import time
+
+start_time = time.time()
 
 with open("input.json", "r") as f:
     data = json.load(f)
 
-
 prof_groups = {}
 prof_preferences = {}
-
 
 for prof, details in data.items():
     prof_groups[prof] = details["group"]
@@ -15,21 +16,32 @@ for prof, details in data.items():
     for course_category, courses in details["courses"].items():
         prof_preferences[prof].extend(courses.values())
 
-
 all_possible_assignments = product(*prof_preferences.values())
-print(all_possible_assignments)
 
 count = 0
+valid_assignments = []
+
 for assignment in all_possible_assignments:
-    # Initialize the course loads
     course_loads = {prof: 0 for prof in prof_groups}
 
     
     for prof, course in zip(prof_groups, assignment):
-        course_loads[prof] += 0.5  # Each course is split into 0.5
+        course_loads[prof] += 0.5 
 
     
     if all(0 <= course_loads[prof] <= prof_groups[prof] for prof in prof_groups):
         count += 1
-        print(f"Valid assignment: {dict(zip(prof_groups, assignment))}")
-print(count)
+        valid_assignments.append(dict(zip(prof_groups, assignment)))
+
+end_time = time.time()
+print(f"Time taken: {end_time - start_time} seconds\n\n")
+
+
+output_data = {
+    "valid_assignments": valid_assignments,
+    "number_of_valid_assignments": count,
+    "time_taken": end_time - start_time
+}
+
+with open("output.json", "w") as json_file:
+    json.dump(output_data, json_file, indent=2)
